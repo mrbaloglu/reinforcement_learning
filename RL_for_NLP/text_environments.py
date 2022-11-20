0ceb32da47491e43a1c59a5e3f9b036ac5a824ef
emrebaloglu <emre.baloglu@soostone.com>
2022-11-20 10:58:22 +0300

    fixes

--- RL_for_NLP/text_environments.py
+++ RL_for_NLP/text_environments.py
@@ -194,12 +194,11 @@ class TextEnvClfBert(gym.Env):
         
         action_str = self.action_space.ix_to_action(action)
         label_str = self.action_space.ix_to_action(self.current_label.item())
-        # step_reward = self.reward_function(action_str, label_str, self.prediction_history, self.target_history, self.time_step+1)
         reward, self.confusion_matrix = self.reward_function(action_str, label_str, self.confusion_matrix, self.last_reward, self.time_step+1)
         step_reward = reward - self.last_reward
         self.last_reward = reward
         
-        if action_str in self.pool.possible_actions: # action_str == "good" or "bad":
+        if action_str in self.pool.possible_actions: # e.g action_str == "good" or "bad":
             if self.current_sample_ix != None:
                 self.current_sample_ix += 1
                 self.current_sample_ix %= len(self.pool)
--- RL_for_NLP/text_reward_functions.py
+++ RL_for_NLP/text_reward_functions.py
@@ -8,37 +8,11 @@ class PartialReadingRewardFunctionF1:
     """
     Computes F1 score between predicted and target labels
     It is a step-wise reward function
-    Reward at time step t = score(t) - score(t-1)
+    Reward at time step t = score(t) - score(t-1) (handled in environment code)
     """
     def __init__(self, pos_label: str, label_list: List[str]):
         self.pos_label = pos_label
         self.label_list = label_list
-
-    """def __call__(self, action: str, target, prediction_history, prev_targets: List[str], exploration_discount: float = 0.) -> float:
-
-        if action in self.label_list:
-            # get current action sequence
-            current_pred_history = copy.deepcopy(prediction_history)
-            current_pred_history.append(action)
-
-            current_targets = copy.deepcopy(prev_targets)
-            current_targets.append(target)
-
-            
-            # step reward as change in the scores
-            # as good actions lead to increase in the scores
-            
-            previous_score = 0.
-            if len(prev_targets) > 0:
-                previous_score = f1_score(prev_targets, prediction_history, pos_label=self.pos_label, zero_division=0)
-            
-            current_score = f1_score(current_targets, current_pred_history, pos_label=self.pos_label, zero_division=0)
-            reward = current_score - previous_score
-
-            return reward
-        # give negative feedback when reread, previous or next, here curiosity may be applied
-        else:
-            return  -0.1*(np.log2(exploration_discount))"""
 
     def __call__(self, action: str, target: str, confusion_matrix: np.ndarray, prev_reward, exploration_discount: float = 0.) -> float:
         if action in self.label_list:
