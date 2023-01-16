72ee2459207c38636d87a84e61466ebe0caefa58
Emre Baloglu <mrbaloglu@gmail.com>
2023-01-16 11:53:46 +0000

    Main

--- RL_for_NLP/Text Classification/imdb/tests_on_imdb.py
+++ RL_for_NLP/Text Classification/imdb/tests_on_imdb.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 import sys
 sys.path.append("/Users/emrebaloglu/Documents/RL/basic_reinforcement_learning")
-from RL_for_NLP.text_environments import TextEnvClfBert, TextEnvClf
+from RL_for_NLP.text_environments import TextEnvClfForBertModels
 from RL_for_NLP.text_data_pools import PartialReadingDataPoolWithTokens, PartialReadingDataPoolWithBertTokens
 from RL_for_NLP.text_reward_functions import calculate_stats_from_cm
 import NLP_utils.preprocessing as nlp_processing
@@ -30,33 +30,35 @@ from collections import Counter
 from tqdm import tqdm
 
 
-with open("/Users/emrebaloglu/Documents/RL/basic_reinforcement_learning/NLP_datasets/IMDB_reviews/data_info_bert.json", "r") as f:
+with open("/Users/emrebaloglu/Documents/RL/basic_reinforcement_learning/NLP_datasets/imdb/bert-base-uncased_data_info.json", "r") as f:
     data_info = json.load(f)
 
-data_train = nlp_processing.openDfFromPickle(data_info["path"] + "/imdb-train-bert.pkl")
+
+data_train = nlp_processing.openDfFromPickle("NLP_datasets/imdb/imdb_train_bert-base-uncased.pkl")
 
 
 
 
 # declare some hyperparameters
 WINDOW_SIZE = 64
+MAX_LEN = data_info["max_len"]
 MAX_STEPS = int(1e+5)
-TRAIN_STEPS = int(1e+3)
+TRAIN_STEPS = int(1e+4)
 VOCAB_SIZE = data_info["vocab_size"]
 REWARD_FN = "score"
-print(f"Vocab size: {VOCAB_SIZE}")
+print(f"Vocab size: {VOCAB_SIZE}, maximum sentence length: {MAX_LEN}")
 
-data_test = nlp_processing.openDfFromPickle(data_info["path"] + "/imdb-test-bert.pkl")
-data_val = nlp_processing.openDfFromPickle(data_info["path"] + "/imdb-val-bert.pkl")
 
-train_pool = PartialReadingDataPoolWithBertTokens(data_train, "review", "label", WINDOW_SIZE)
-test_pool = PartialReadingDataPoolWithBertTokens(data_test, "review", "label", WINDOW_SIZE)
-val_pool = PartialReadingDataPoolWithBertTokens(data_val, "review", "label", WINDOW_SIZE)
+data_test = nlp_processing.openDfFromPickle("NLP_datasets/imdb/imdb_test_bert-base-uncased.pkl")
+data_val = nlp_processing.openDfFromPickle("NLP_datasets/imdb/imdb_val_bert-base-uncased.pkl")
 
-train_env_params = dict(data_pool=train_pool, max_time_steps=MAX_STEPS, reward_fn=REWARD_FN, vocab_size=VOCAB_SIZE)
-train_env = TextEnvClfBert(**train_env_params)
-val_env = TextEnvClfBert(val_pool, 5000, VOCAB_SIZE, reward_fn=REWARD_FN)
-test_env = TextEnvClfBert(test_pool, 1000, VOCAB_SIZE, reward_fn=REWARD_FN)
+train_pool = PartialReadingDataPoolWithBertTokens(data_train, "text", "label", MAX_LEN, WINDOW_SIZE, mask = True)
+test_pool = PartialReadingDataPoolWithBertTokens(data_test, "text", "label", MAX_LEN, WINDOW_SIZE, mask = True)
+val_pool = PartialReadingDataPoolWithBertTokens(data_val, "text", "label", MAX_LEN, WINDOW_SIZE, mask = True)
+
+train_env = TextEnvClfForBertModels(train_pool, VOCAB_SIZE, MAX_STEPS, reward_fn=REWARD_FN)
+val_env = TextEnvClfForBertModels(val_pool, VOCAB_SIZE, 5000, reward_fn=REWARD_FN)
+test_env = TextEnvClfForBertModels(test_pool, VOCAB_SIZE, 1000, reward_fn=REWARD_FN)
 print("All environments are created.")
 
 device = torch.device("cuda:0") # if torch.cuda.is_available() else "cpu")
@@ -99,13 +101,12 @@ def eval_model(model, env, total_timesteps=10000):
 )"""
 
 policy_kwargs = dict(
-    features_extractor_class=pn.RNNExtractor,
-    features_extractor_kwargs=dict(vocab_size = VOCAB_SIZE, embed_dim = 4,
-                 rnn_type = "lstm", rnn_hidden_size = 1, rnn_hidden_out = 2, rnn_bidirectional = True,
-                 features_dim = 2, units = 16),
+    features_extractor_class=pn.DistilbertExtractor,
+    features_extractor_kwargs=dict(window_size=WINDOW_SIZE, features_dim=32),
 )
 # policy_kwargs = dict(net_arch=[dict(pi=[512, 256, 64, 64], qf=[400, 300])])
-model = DQN("MlpPolicy", train_env, policy_kwargs=policy_kwargs, verbose=0, batch_size=64)
+model = DQN("MultiInputPolicy", train_env, policy_kwargs=policy_kwargs, verbose=0, batch_size=64)
+# model.learn(10000)
 # model = A2C(policy = "MlpPolicy",
 #             env = train_env,
 #             gae_lambda = 0.9,
@@ -120,23 +121,20 @@ model = DQN("MlpPolicy", train_env, policy_kwargs=policy_kwargs, verbose=0, batc
 #             verbose=0, 
 #             use_rms_prop=True, device=device)
 # model = PPO("MlpPolicy", train_env, policy_kwargs=policy_kwargs)
-train_env.set_train_mode(False)
-val_env.set_train_mode(False)
-test_env.set_train_mode(False)
-"""for i in range(int(250)):
-    model.learn(total_timesteps=TRAIN_STEPS, reset_num_timesteps=True, progress_bar=True)
-    train_env.set_train_mode(False)
+
+for i in range(int(2)):
+    model.learn(total_timesteps=TRAIN_STEPS*6, reset_num_timesteps=True, progress_bar=True)
+    
     print("======= On train env: ===============")
     eval_model(model, train_env)
     print("======= On val env: ===============")
     eval_model(model, val_env)
-    train_env.set_train_mode(True)
-"""
-model = pn.RNN_Baseline_Policy(VOCAB_SIZE, WINDOW_SIZE, 3)
-optim = torch.optim.Adam(model.parameters(), lr = 0.0001)
 
-rl_monte_carlo.reinforce_algorithm(train_env, model, optim, 10, 500, 0.99)
+# model = pn.RNN_Baseline_Policy(VOCAB_SIZE, WINDOW_SIZE, 3)
+# optim = torch.optim.Adam(model.parameters(), lr = 0.0001)
+
+# rl_monte_carlo.reinforce_algorithm(train_env, model, optim, 10, 500, 0.99)
 print("======= On train env: ===============")
-eval_model(model, train_env)
+# eval_model(model, train_env)
 print("======= On val env: ===============")
 eval_model(model, val_env)
--- actor_critic_algortihm_utils.py
+++ actor_critic_algortihm_utils.py
@@ -6,12 +6,14 @@ from torch.distributions import Categorical
 import numpy as np
 
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, Dict, Tuple
 from collections import namedtuple
 from itertools import count
 import gym
 from tqdm import tqdm
+from transformers import DistilBertModel
 
+from RL_for_NLP.text_reward_functions import calculate_stats_from_cm
 
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
@@ -61,6 +63,53 @@ class DenseActorCriticPolicy(ActorCriticPolicy):
         x = self.dropout_layer(x)
         x = F.relu(self.affine2(x))
         x = F.relu(self.affine3(x))
+        # actor: choses action to take from state s_t
+        # by returning probability of each action
+        action_prob = F.softmax(self.action_head(x), dim=-1)
+
+        # critic: evaluates being in the state s_t
+        state_values = self.value_head(x)
+
+        # return values for both actor and critic as a tuple of 2 values:
+        # 1. a list with the probability of each action over the action space
+        # 2. the value from state s_t
+        return action_prob, state_values
+
+class DistibertActorCriticPolicy(ActorCriticPolicy):
+    def __init__(self, state_dim: int, action_dim: int, use_last_n_layers: int = 0, dropout = 0.4, bert_model = None):
+        super().__init__(state_dim=state_dim, action_dim=action_dim, dropout=dropout)
+        
+        if bert_model == None:
+            self.bert = DistilBertModel.from_pretrained("distilbert-base-uncased")
+        else: 
+            self.bert = bert_model
+        """n_distilbert_param = len([x for x in self.distilbert.parameters()])
+        cnt = 0
+        for param in self.distilbert.parameters():
+            if cnt < use_last_n_layers:
+                param.requires_grad = False
+            cnt += 1"""
+        self.distilbert.requires_grad = False
+        
+        
+        self.flatten = nn.Flatten()
+        self.dense = nn.Linear(self.state_dim*768, 128)
+        self.dropout_layer = nn.Dropout(p=self.dropout)
+        # actor's layer
+        self.action_head = nn.Linear(128, self.action_dim)
+
+        # critic's layer
+        self.value_head = nn.Linear(128, 1)
+
+        
+
+    def forward(self, x_input_id: th.tensor, x_attn_mask: th.tensor) -> th.Tensor:
+        
+        x = self.bert(x_input_id, x_attn_mask).last_hidden_state
+        
+        x = self.flatten(x)
+        x = F.relu(self.dense(x))
+        x = self.dropout_layer(x)
         # actor: choses action to take from state s_t
         # by returning probability of each action
         action_prob = F.softmax(self.action_head(x), dim=-1)
@@ -147,17 +196,18 @@ class ActorCriticAlgorithm:
 
     def train_a2c(self, n_episodes: int, n_steps: int, render_env: bool = False, log_interval: int = 150):
         running_reward = 10
-        
+        last_reward = 0
         # run infinitely many episodes / number of episodes selected
-        for i_episode in range(1, n_episodes + 1):
-
+        pbar = tqdm(range(1, n_episodes+1))
+        for i_episode in pbar:
+            pbar.set_description(f"Average reward so far: {last_reward:.3f} (updated every {log_interval} episodes)")
             # reset environment and episode reward
             state = self.env.reset()
             ep_reward = 0
 
             # for each episode, only run 9999 steps so that we don't
             # infinite loop while learning
-            for t in range(1, n_steps):
+            for t in tqdm(range(1, n_steps), leave=False):
 
                 # select action from policy
                 action = self.select_action(state)
@@ -176,23 +226,141 @@ class ActorCriticAlgorithm:
             # update cumulative reward
             running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
 
+            
             # perform backprop
             self.finish_episode()
 
             # log results
             if i_episode % log_interval == 0:
-                print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                    i_episode, ep_reward, running_reward))
+                # print(f'Episode {i_episode}\tLast reward: {ep_reward:.2f}\tAverage reward: {running_reward:.2f}')
+                last_reward = running_reward
 
-            # check if we have "solved" the cart pole problem
+            """# check if we have "solved" the cart pole problem
             if running_reward > self.env.spec.reward_threshold:
                 print("Solved! Running reward is now {} and "
                     "the last episode runs to {} time steps!".format(running_reward, t))
-                break
+                break"""
+        
+
+class ActorCriticAlgorithmBertModel:
+    def __init__(self, policy: ActorCriticPolicy, env: gym.Env, optimizer: th.optim, gamma: float = 0.99, **kwargs):
+        self.policy = policy
+        self.env = env
+        self.gamma = gamma
+        self.optimizer = optimizer
     
+    
+
+    # select an action given a state from env, using the policy
+    def select_action(self, state: Dict[str, np.ndarray]):
+        input_ids = th.from_numpy(state["input_id"]).unsqueeze(0)
+        attn_mask = th.from_numpy(state["attn_mask"]).unsqueeze(0)
+        
+        probs, state_value = self.policy(input_ids, attn_mask)
+        m = Categorical(probs)
+        action = m.sample()
+        # save to action buffer
+        self.policy.saved_actions.append(SavedAction(m.log_prob(action), state_value))
+        self.policy.saved_log_probs.append(m.log_prob(action))
+        return action.item()
+
+     
+ 
+    def finish_episode(self, epsilon: float = np.finfo(np.float32).eps.item()): # epsilon > 0 to prevent divide by 0 
+        """
+        Training code. Calculates actor and critic loss and performs backprop.
+        """
+        R = 0
+        saved_actions = self.policy.saved_actions
+        policy_losses = [] # list to save actor (policy) loss
+        value_losses = [] # list to save critic (value) loss
+        returns = [] # list to save the true values
+
+        # calculate the true value using rewards returned from the environment
+        for r in self.policy.rewards[::-1]:
+            # calculate the discounted value
+            R = r + self.gamma * R
+            returns.insert(0, R)
+
+        returns = th.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + epsilon)
+
+        for (log_prob, value), R in zip(saved_actions, returns):
+            advantage = R - value.item()
+
+            # calculate actor (policy) loss
+            policy_losses.append(-log_prob * advantage)
+
+            # calculate critic (value) loss using L1 smooth loss
+            value_losses.append(F.smooth_l1_loss(value, th.tensor([R])))
+
+        # reset gradients
+        self.optimizer.zero_grad()
+
+        # sum up all the values of policy_losses and value_losses
+        loss = th.stack(policy_losses).sum() + th.stack(value_losses).sum()
+
+        # perform backprop
+        loss.backward()
+        self.optimizer.step()
+
+        # reset rewards and action buffer
+        del self.policy.rewards[:]
+        del self.policy.saved_actions[:]
+
+    def train_a2c(self, n_episodes: int, n_steps: int, render_env: bool = False, log_interval: int = 150):
+        running_reward = 0.
+        last_reward = 0
+        # run infinitely many episodes / number of episodes selected
+        pbar = tqdm(range(1, n_episodes+1))
+        for i_episode in pbar:
+            pbar.set_description(f"Stats:  {calculate_stats_from_cm(self.env.confusion_matrix)}, Average reward: {last_reward:.3f} (updated every {log_interval} episodes)")
+            # reset environment and episode reward
+            state = self.env.reset()
+            ep_reward = 0
+
+            # for each episode, only run 9999 steps so that we don't
+            # infinite loop while learning
+            for t in tqdm(range(1, n_steps), leave=False):
+
+                # select action from policy
+                action = self.select_action(state)
+
+                # take the action
+                state, reward, done, _, = self.env.step(action)
+
+                if render_env:
+                    self.env.render()
+
+                self.policy.rewards.append(reward)
+                ep_reward += reward
+                if done:
+                    break
+
+            # update cumulative reward
+            running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+
+            
+            # perform backprop
+            self.finish_episode()
+
+            # log results
+            if i_episode % log_interval == 0:
+                # print(f'Episode {i_episode}\tLast reward: {ep_reward:.2f}\tAverage reward: {running_reward:.2f}')
+                last_reward = ep_reward
+            
+            print(f"Observe: {_}")
+
+            """# check if we have "solved" the cart pole problem
+            if running_reward > self.env.spec.reward_threshold:
+                print("Solved! Running reward is now {} and "
+                    "the last episode runs to {} time steps!".format(running_reward, t))
+                break"""
+        
+
 if __name__ == "__main__":
 
-    env = gym.make('CartPole-v1')
+    env = gym.make('MountainCar-v0')
     print(env.observation_space, env.observation_space.shape)
     print(env.action_space, env.action_space.shape)
     print(env.reset())
@@ -202,4 +370,4 @@ if __name__ == "__main__":
     optimizer = th.optim.Adam(model.parameters(), lr=3e-2)
     
     algo = ActorCriticAlgorithm(model, env, optimizer)
-    algo.train_a2c(10000, 1000)
+    algo.train_a2c(10000, 1000, log_interval=10)
